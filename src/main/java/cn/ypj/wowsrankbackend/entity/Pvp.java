57820b264b59d724883854fcc872728f3221a2c8
SangonomiyaKoko <3197206779@qq.com>
2023-04-01 09:29:20 +0800

    删除无用数据

--- src/main/java/cn/ypj/wowsrankbackend/entity/Pvp.java
+++ src/main/java/cn/ypj/wowsrankbackend/entity/Pvp.java
@@ -4,52 +4,82 @@ import lombok.Data;
 
 @Data
 public class Pvp{
-    private int maxFragsByMain; // 主炮最多摧毁敌舰的次数
-    private int hitsByAtba; // 高射炮击中次数
-    private int shotsByMain; // 主炮发射次数
-    private int maxFragsByMainVehicle; // 主炮摧毁敌舰次数最多的舰船ID
-    private int maxFragsByTpdVehicle; // 鱼雷摧毁敌舰次数最多的舰船ID
-    private int shipsSpotted; // 发现敌舰的数量
-    private int maxScoutingDamage; // 最高侦察伤害值
-    private int premiumExp; // 高级账户获得的经验总数
-    private int droppedCapturePoints; // 摧毁敌方占领点的数量
-    private int maxFragsVehicle; // 摧毁敌舰次数最多的舰船ID
-    private int maxFragsByDbomb; // 深水炸弹摧毁敌舰次数最多的舰船ID
-    private int controlCapturedPoints; // 控制的占领点数量
-    private int maxPremiumExpVehicle; // 高级账户经验最多的舰船ID
-    private int maxPlanesKilled; // 最多击落敌方飞机的数量
-    private int maxExpVehicle; // 经验值最多的舰船ID
-    private int battlesCount; // 总战斗次数
-    private int planesKilled; // 击落的敌方飞机数量
-    private int fragsByTpd; // 鱼雷摧毁敌舰次数
-    private int maxShipsSpotted; // 发现敌舰数量最多的舰船ID
-    private int maxFragsByRam; // 通过撞击摧毁敌舰的最大次数
-    private int maxTotalAgroVehicle; // 承受敌方火力最多的舰船ID
-    private int maxFragsByRamVehicle; // 通过撞击摧毁敌舰最多的舰船ID
-    private int maxPlanesKilledVehicle; // 击落敌方飞机数量最多的舰船ID
-    private int maxDamageDealtVehicle; // 造成伤害最多的舰船ID
-    private int maxFragsByDbombVehicle; // 深水炸弹摧毁敌舰次数最多的舰船ID
-    private int survived; // 存活的场次
-    private int scoutingDamage; // 侦察伤害总数
-    private int maxShipsSpottedVehicle; // 发现敌舰数量最多
-    private int fragsByAtba; // 高射炮摧毁敌舰次数
-    private int controlDroppedPoints; // 丢失控制的占领点数量
-    private int maxDamageDealt; // 最大单场造成伤害
-    private int maxFragsByTpd; // 鱼雷最多摧毁敌舰次数
-    private int maxPremiumExp; // 单场最高高级账户经验
-    private int hitsByTpd; // 鱼雷击中次数
-    private int maxFragsByAtba; // 高射炮最多摧毁敌舰次数
-    private int tpdAgro; // 鱼雷吸引敌方火力的次数
-    private int maxScoutingDamageVehicle; // 侦察伤害最高的舰船ID
-    private int teamControlCapturedPoints; // 团队共同控制的占领点数量
-    private int wins; // 胜利场次
-    private int losses; // 失败场次
-    private int hitsByMain; // 主炮击中次数
-    private int winAndSurvived; // 胜利且存活的场次
-    private int damageDealt; // 造成的总伤害
-    private int fragsByMain; // 主炮摧毁敌舰次数
-    private int teamControlDroppedPoints; // 团队失去控制的占领点数量
-    private int maxFrags; // 单场最多击杀次数
-    private int exp; // 总获得经验值
-    private int maxExp; // 单场最高经验值
+
+    //主要数据
+    private int battlesCount;               //战斗总数
+    private int wins;                       //胜利场数
+    private int losses;                     //失败场数
+    private int damageDealt;                //总伤害
+    private int shipsSpotted;               //船只侦查数
+    private int frags;                      //总击杀数
+    private int survived;                   //存活的场数
+    private int scoutingDamage;             //总侦查伤害
+    private int originalExp;                //总经验（无高账加成）
+    private int exp;                        //经验（大概是包括所有模式的总经验，虽然没啥用，但留着吧）
+    private int artAgro;                    //总潜在伤害
+    private int tpdAgro;                    //总鱼雷潜在伤害
+    private int winAndSurvived;             //胜利并存活场数
+    private int controlDroppedPoints;       //总防御点数
+    private int controlCapturedPoints;      //总占领点数
+    private int teamControlCapturedPoints;  //团队总占领点数
+    private int teamControlDroppedPoints;   //团队总防御点数
+    private int planesKilled;               //总飞机击落数
+
+    //最大记录
+    private int maxFragsByPlanes;           //最多通过飞机击杀数
+    private int maxTotalAgro;               //最多潜在伤害
+    private int maxShipsSpotted;            //最多船只侦查数
+    private int maxFragsByRam;              //最多冲撞击杀
+    private int maxScoutingDamage;          //最大侦查伤害
+    private int maxFragsByDbomb;            //最多深水炸弹击杀
+    private int maxFragsByMain;             //最多主炮击杀
+    private int maxPlanesKilled;            //最多飞机击落
+    private int maxDamageDealt;             //最大伤害
+    private int maxFragsByTpd;              //最多鱼雷击杀
+    private int maxExp;                     //最多经验（无高账加成）
+    private int maxFragsByAtba;             //最多副炮击杀数
+    private int maxFrags;                   //最多击杀数
+
+    //武器击杀数据
+    private int fragsByRam;                 //冲撞击杀数
+    private int fragsByTpd;                 //鱼雷击杀数
+    private int fragsByPlanes;              //通过飞机击杀数
+    private int fragsByDbomb;               //深水炸弹的击杀数
+    private int fragsByAtba;                //副炮击杀数
+    private int fragsByMain;                //主炮击杀数
+    //通过起火或进水击杀 = 总击杀 - 上面的累加
+
+    //武器命中数据
+    private int hitsByMain;                 //命中的主炮数
+    private int shotsByMain;                //发射的主炮数
+    private int hitsBySkip;                 //命中的跳弹数
+    private int shotsBySkip;                //发射的跳弹数
+    private int hitsByAtba;                 //命中的副炮数
+    private int shotsByAtba;                //发射的副炮数
+    private int hitsByRocket;               //命中的火箭弹数
+    private int shotsByRocket;              //发射的火箭弹数
+    private int hitsByBomb;                 //命中的炸弹数
+    private int shotsByBomb;                //投掷的炸弹数
+    private int hitsByTpd;                  //命中的鱼雷数
+    private int shotsByTpd;                 //发射的鱼雷数
+    private int hitsByTbomb;                //命中的空袭炸弹数
+    private int shotsByTbomb;               //投掷的空袭炸弹数
+
+    //删除的数据
+    /*
+    "damage_dealt_to_buildings": 0,         对建筑伤害总量
+    "max_damage_dealt_to_buildings": 0,     最大对建筑伤害
+    "max_premium_exp": 4076,                最大经验（有高账加成）
+    "premium_exp": 156963,                  总经验（有高账加成）
+    "dropped_capture_points": 0,            ......(什么勾八)
+    "capture_points": 0,                    ......(什么勾八)
+    "max_suppressions_count": 0,            ......(什么勾八)
+    "suppressions_count": 0,                ......(什么勾八)
+    "battles_count_0910": 74,               0.9.10版本后战斗场数
+    "battles_count_078": 74,                0.7.8版本后战斗场数
+    "battles_count_0711": 74,               0.7.11版本后的战斗场数
+    "battles_count_512": 74,                0.5.12版本后的战斗场数
+    "battles_count_510": 74,                0.5.10版本后的战斗场数
+     */
+
 }
